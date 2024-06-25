6c58d9691a9d92ef34932777dc251f0f7b78a5be
jiaweiWang25414 <wangjiawei@sjtu.edu.cn>
2024-06-25 15:10:15 +0800

    BackEnd

---
+++ BackEnd/src/main/java/org/example/summer/SummerApplication.java
@@ -0,0 +1,13 @@
+package org.example.summer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/org/example/summer/SummerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.summer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
